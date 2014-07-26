c5fa77d3954dc7c79b07ee5309430c446d0b2ca8
MightyPork <ondra@ondrovo.com>
2014-07-26 14:49:55 +0200

    some javadoc fixes

--- src/mightypork/gamecore/backend/Backend.java
+++ src/mightypork/gamecore/backend/Backend.java
@@ -8,22 +8,22 @@ import mightypork.utils.eventbus.clients.BusNode;
 
 /**
  * Application backend interface (set of core modules).<br>
- * Backend is created without a bus access, which will be assigned during app
- * initialization.
+ * The goal of this abstraction is to allow easy migration to different
+ * environment with different libraries etc. It should be as simple as using
+ * different backend.
  * 
  * @author MightyPork
  */
 public abstract class Backend extends BusNode {
 	
 	/**
-	 * Initialize backend modules, add them to event bus.<br>
-	 * Event bus must already be available in the BusAccess.
+	 * Initialize backend modules, add them to event bus.
 	 */
 	public abstract void initialize();
 	
 	
 	/**
-	 * Get graphics module (renderer)
+	 * Get graphics module (screen manager, texture and font loader, renderer)
 	 * 
 	 * @return graphics module
 	 */
@@ -31,9 +31,9 @@ public abstract class Backend extends BusNode {
 	
 	
 	/**
-	 * Get graphics module (renderer)
+	 * Get audio module (
 	 * 
-	 * @return graphics module
+	 * @return audio module
 	 */
 	public abstract AudioModule getAudio();
 }
